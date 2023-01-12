8d81f67cfb65e23216ec2041b9c60c209a2b0585
Lucas Vinicius <lucas_vfsantos@hotmail.com>
2023-01-12 19:40:01 -0300

    Removendo parenteses

--- app/aula7/desafio10.py
+++ app/aula7/desafio10.py
@@ -1,4 +1,4 @@
 # conversão de temperatura ºC para ºF
 celcius: float = float(input('Informe a temperatura em ºC: '))
-fahrenheit = (celcius * 9/5) + 32
+fahrenheit = celcius * 9 / 5 + 32
 print('A temperatura de {:.2f}ºC corresponde a {:.2f}ºF!'.format(celcius, fahrenheit))
