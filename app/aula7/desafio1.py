8fc0f89408d7ba7bf59595ed672ca35b57c3d2ff
Lucas Vinicius <lucas_vfsantos@hotmail.com>
2023-01-12 19:26:55 -0300

    Cria arquivo desafio1

---
+++ app/aula7/desafio1.py
@@ -0,0 +1,7 @@
+# Sucessor e Antecessor
+numero: int = int(input('Digite um número '))
+sucessor = numero + 1
+antecessor = numero - 1
+print('Você digitou o número {} o antecessor dele é {} e o sucessor é {}'.format(numero, antecessor, sucessor))
+# numero: int = int(input('Digite um número '))
+# print('Você digitou o número {} o antecessor dele é {} e o sucessor é {}'.format(numero, (numero - 1), (numero + 1)))
