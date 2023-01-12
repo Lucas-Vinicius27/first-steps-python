9e8bdc18c8dfd3fe1fa9e339b96fb910f735aac6
Lucas Vinicius <lucas_vfsantos@hotmail.com>
2023-01-12 19:27:19 -0300

    Cria desafio 3

---
+++ app/aula7/desafio3.py
@@ -0,0 +1,5 @@
+# media de dois números
+n1: float = float(input('Digite a primeira nota do aluno '))
+n2: float = float(input('Digite a segunda nota do aluno '))
+media = (n1 + n2) / 2
+print('A media do aluno é {:.2f}'.format(media))
